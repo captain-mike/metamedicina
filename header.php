efa7298402a5374ac83be8f33dc0b8fb91fccad4
captain-mike <info@grafikweb.it>
2022-03-28 17:53:50 +0200

    header first styles

--- header.php
+++ header.php
@@ -1,27 +1,48 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> <?php metamedicina_schema_type(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bubbler+One&family=Montserrat+Alternates:wght@300;400&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<div id="branding">
-<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-<?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
-echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
-?>
-</div>
-<div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
-</div>
-<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
-<div id="search"><?php get_search_form(); ?></div>
-</nav>
-</header>
-<div id="container">
+    <?php wp_body_open(); ?>
+    <div id="wrapper" class="hfeed">
+        <header id="header" role="banner">
+            <div id="top-bar" class="grey_bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4 email-col">
+                            Contattaci: <a href="mailto:italia@metamedecine.com">italia@metamedecine.com</a>
+                        </div>
+                        <div class="col-8 language-col">
+                            <?php do_action('wpml_add_language_selector'); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="main-bar" class="red_bg py-4">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4">
+                            <div id="main-logo" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+                                <img src="<?=get_template_directory_uri()?>/img/logo-metamedicina.png" alt="">
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+                                <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <div id="container">
+            
+           
