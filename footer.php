b0594f481f9735db528eedf5b1f54bd49dfd51a5
captain-mike <info@grafikweb.it>
2022-03-28 15:46:22 +0200

    footer first styles

--- footer.php
+++ footer.php
@@ -1,11 +1,42 @@
-<?php get_sidebar(); ?>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'metamedicina' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div id="disclaimer">
+        <div class="container grey_bg text-center">
+            <small>
+                <b>La Metamedicina non intende sostituirsi ai percorsi indicati dalla medicina ufficiale né sostituire alcuna terapia psicologica.</b><br>
+                Può invece rivelarsi molto efficace come sostegno ed integrazione a un percorso evolutivo personale. I consulenti di Metamedicina non svolgono attività diagnostica, non prescrivono farmaci e non si sostituiscono alle figure ufficiali di riferimento; accompagnano la persona a prendere coscienza delle proprie dinamiche emozionali, fisiche e mentali attraverso le chiavi della Metamedicina per tornare in accordo con sè stessi.
+            </small>
+        </div>
+    </div>
+    <div id="newsletter" class="py-5 red_bg">
+        <div class="container">
+            <div class="col offset-0 col-md-6 offset-3 mb-3">
+                Sei interessato a sapere tutte le novità della Metamedicina e a tenerti informato sui corsi in partenza?
+                Iscriviti alla nostra newsletter a cadenza mensile!
+            </div>
+            <div class="col offset-0 col-md-6 offset-3 mb-3">
+                <input type="text" required placeholder="nome" class="form-control" id="name">
+            </div>
+            <div class="col offset-0 col-md-6 offset-3 mb-3">
+                <input type="email" required placeholder="email" class="form-control" id="psw">
+            </div>
+            <div class="col offset-0 col-md-6 offset-3">
+                <div class="btn btn-primary">Iscriviti</div>
+            </div>
+        </div>
+    </div>
+    <div id="copyright">
+        <div class="container grey_bg text-center py-3">
+            <div class="custom">
+                <a href="/privacy-policy.html">PRIVACY POLICY</a> | <a href="/cookie-policy.html">COOKIE POLICY</a>
+                <p >Copyright © 2018 Metamedicina di Claudia Rainville • Note legali • Tutti i diritti riservati</p>
+                <small>Sito web realizzato da <a href="http://wp-admin.it/" target="_blank" rel="noopener noreferrer" title="Siti Web a Reggio Emilia">WP-Admin</a></small>
+            </div>
+        </div>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
 </body>
+
 </html>
