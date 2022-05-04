8eb79c49a5e36695ae8281884c040caf84304b7c
captain-mike <info@grafikweb.it>
2022-05-05 01:42:00 +0200

    fix

--- templates/calendar-template.php
+++ templates/calendar-template.php
@@ -12,7 +12,7 @@ get_header(); ?>
 </div>
 
 <h1 class="page-title py-2 red_bg mt-4 mb-2">
-    <?php the_title() ?> <?php _('incoming', 'metamedicina')?>
+    <?php the_title() ?> <?php _e('incoming', 'metamedicina')?>
 </h1>
 
 <section id="calendar-icons" class="my-lg-5 py-5">
@@ -59,7 +59,7 @@ get_header(); ?>
             <a href="<?php the_permalink()?>" class="col-12 col-lg-8 calendar-details">
                 <h3 class="event-title"><?php the_title()?></h3>
                 <div class="event-trainer grey_bg mb-2">
-                    <?php _('Conducted by', 'metamedicina')?> 
+                    <?php _e('Conducted by', 'metamedicina')?> 
                     <b><?=get_field('trainer')[0]->post_title ?></b> 
                     <i class="bi bi-person-video3"></i>
                 </div>
